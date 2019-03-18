c2d83bbb91eafb0dfd809587c95b16e4ce629f49
Jamie Goodnight <goodnightlambda@gmail.com>
2019-03-18 15:36:38 -0400

    Return all users.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(5000, () => {
+    console.log('\n** API up and running on port 5K')
+});
+
+server.get('/', (req, res) => {
+    res.send('Hello, Jamie')
+});
+
+
+// Get - Return all users in db 
+
+server.get('/users', (req, res) => {
+    db
+    .find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).json({message: 'The users information could not be retrieved.'})
+    })
+})  
+
+
